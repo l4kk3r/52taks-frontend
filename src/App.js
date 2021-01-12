50ec64e07f2f68841d82d71ae30b7985c755b006
Tim <l4kk3r@yandex.ru>
2021-01-12 14:00:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route, useHistory, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import Admin from './components/Admin'
+
+const Routing = () => {
+  return(
+    <Switch>
+      <Route path='/' component = {Home} exact />
+      <Route path='/admin' component = {Admin} exact />
+    </Switch>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
+  );
+}
+
+export default App;
